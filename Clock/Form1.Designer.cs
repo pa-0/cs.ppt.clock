5142979599f2db1b6059c93ade8f7b9ff1e48d0d
Eduard Spack <edi.spack@gmail.com>
2020-01-16 14:38:01 +0100

    Initial commit

---
+++ Clock/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Clock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.closeButton = new System.Windows.Forms.Button();
+            this.pinButton = new System.Windows.Forms.Button();
+            this.biggerButton = new System.Windows.Forms.Button();
+            this.smallerButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.OnTick);
+            // 
+            // closeButton
+            // 
+            this.closeButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.closeButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.closeButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.closeButton.FlatAppearance.BorderSize = 0;
+            this.closeButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.closeButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.closeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.closeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.closeButton.ForeColor = System.Drawing.Color.Red;
+            this.closeButton.Location = new System.Drawing.Point(370, 0);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(30, 30);
+            this.closeButton.TabIndex = 0;
+            this.closeButton.UseVisualStyleBackColor = false;
+            this.closeButton.Visible = false;
+            this.closeButton.Click += new System.EventHandler(this.OnClickClose);
+            this.closeButton.MouseEnter += new System.EventHandler(this.OnMouseEnterClose);
+            this.closeButton.MouseLeave += new System.EventHandler(this.OnMouseLeaveClose);
+            // 
+            // pinButton
+            // 
+            this.pinButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.pinButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pinButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.pinButton.FlatAppearance.BorderSize = 0;
+            this.pinButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.pinButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.pinButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.pinButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.pinButton.ForeColor = System.Drawing.Color.Blue;
+            this.pinButton.Location = new System.Drawing.Point(335, 0);
+            this.pinButton.Name = "pinButton";
+            this.pinButton.Size = new System.Drawing.Size(30, 30);
+            this.pinButton.TabIndex = 1;
+            this.pinButton.UseVisualStyleBackColor = false;
+            this.pinButton.Visible = false;
+            this.pinButton.Click += new System.EventHandler(this.OnClickPin);
+            this.pinButton.MouseEnter += new System.EventHandler(this.OnMouseEnterPin);
+            this.pinButton.MouseLeave += new System.EventHandler(this.OnMouseLeavePin);
+            // 
+            // biggerButton
+            // 
+            this.biggerButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.biggerButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.biggerButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.biggerButton.FlatAppearance.BorderSize = 0;
+            this.biggerButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.biggerButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.biggerButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.biggerButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.biggerButton.ForeColor = System.Drawing.Color.Green;
+            this.biggerButton.Location = new System.Drawing.Point(370, 35);
+            this.biggerButton.Name = "biggerButton";
+            this.biggerButton.Size = new System.Drawing.Size(30, 30);
+            this.biggerButton.TabIndex = 3;
+            this.biggerButton.UseVisualStyleBackColor = false;
+            this.biggerButton.Visible = false;
+            this.biggerButton.Click += new System.EventHandler(this.OnClickBigger);
+            this.biggerButton.MouseEnter += new System.EventHandler(this.OnMouseEnterBigger);
+            this.biggerButton.MouseLeave += new System.EventHandler(this.OnMouseLeaveBigger);
+            // 
+            // smallerButton
+            // 
+            this.smallerButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.smallerButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.smallerButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.smallerButton.FlatAppearance.BorderSize = 0;
+            this.smallerButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.smallerButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.smallerButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.smallerButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.smallerButton.ForeColor = System.Drawing.Color.Green;
+            this.smallerButton.Location = new System.Drawing.Point(335, 35);
+            this.smallerButton.Name = "smallerButton";
+            this.smallerButton.Size = new System.Drawing.Size(30, 30);
+            this.smallerButton.TabIndex = 4;
+            this.smallerButton.UseVisualStyleBackColor = false;
+            this.smallerButton.Visible = false;
+            this.smallerButton.Click += new System.EventHandler(this.OnClickSmaller);
+            this.smallerButton.MouseEnter += new System.EventHandler(this.OnMouseEnterSmaller);
+            this.smallerButton.MouseLeave += new System.EventHandler(this.OnMouseLeaveSmaller);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(400, 400);
+            this.Controls.Add(this.smallerButton);
+            this.Controls.Add(this.biggerButton);
+            this.Controls.Add(this.pinButton);
+            this.Controls.Add(this.closeButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "Clock";
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.Load += new System.EventHandler(this.OnLoad);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.OnPaint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.OnMouseDownMove);
+            this.MouseEnter += new System.EventHandler(this.OnMouseEnter);
+            this.MouseLeave += new System.EventHandler(this.OnMouseLeave);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.OnMouseMove);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.Button pinButton;
+        private System.Windows.Forms.Button biggerButton;
+        private System.Windows.Forms.Button smallerButton;
+    }
+}
+
---
+++ Clock/Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Clock
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int imgRes = 1000;
+        int clockRes = 400;
+        int sec = 0;
+        int xOffset, yOffset;
+        bool pinned = false;
+        DateTime time;
+        Graphics g;
+
+        private void Redraw()
+        {
+            double x, y, a;
+            g = this.CreateGraphics();
+            Image dialImg = Image.FromFile("assets/Dial.png");
+            Image hourImg = Image.FromFile("assets/Hours.png");
+            Image minuteImg = Image.FromFile("assets/Minutes.png");
+            Image secondImg = Image.FromFile("assets/Seconds.png");
+
+            Graphics clock = Graphics.FromImage(dialImg);
+            
+            this.Width = clockRes;
+            this.Height = clockRes;
+
+            a = 30 * (time.Hour >= 12 ? time.Hour - 12 : time.Hour) + time.Minute / 2;
+            x = imgRes / 2 * (1 + Math.Sqrt(2) * Math.Cos(Math.PI * (0.75 - a / 180)));
+            y = imgRes / 2 * (1 - Math.Sqrt(2) * Math.Sin(Math.PI * (0.75 - a / 180)));
+            clock.TranslateTransform((float)x, (float)y);
+            clock.RotateTransform((float)a);
+            clock.DrawImage(hourImg, 0, 0, imgRes, imgRes);
+
+            a = 6 * time.Minute + time.Second / 10;
+            x = imgRes / 2 * (1 + Math.Sqrt(2) * Math.Cos(Math.PI * (0.75 - a / 180)));
+            y = imgRes / 2 * (1 - Math.Sqrt(2) * Math.Sin(Math.PI * (0.75 - a / 180)));
+            clock.TranslateTransform((float)x, (float)y);
+            clock.RotateTransform((float)a);
+            clock.DrawImage(minuteImg, 0, 0, imgRes, imgRes);
+
+            a = 6 * time.Second;
+            x = imgRes / 2 * (1 + Math.Sqrt(2) * Math.Cos(Math.PI * (0.75 - a / 180)));
+            y = imgRes / 2 * (1 - Math.Sqrt(2) * Math.Sin(Math.PI * (0.75 - a / 180)));
+            clock.TranslateTransform((float)x, (float)y);
+            clock.RotateTransform((float)a);
+            clock.DrawImage(secondImg, 0, 0, imgRes, imgRes);
+
+            g.DrawImage(dialImg, 0, 0, clockRes, clockRes);
+        }
+
+        private void OnLoad(object sender, EventArgs e)
+        {
+            time = DateTime.Now;
+        }
+
+        private void OnPaint(object sender, PaintEventArgs e)
+        {
+            Redraw();
+            closeButton.Image = Image.FromFile("assets/Close.png");
+            if(pinned == false)
+            {
+                pinButton.Image = Image.FromFile("assets/Pin.png");
+            }
+            else
+            {
+                pinButton.Image = Image.FromFile("assets/Unpin.png");
+            }
+            smallerButton.Image = Image.FromFile("assets/Smaller.png");
+            biggerButton.Image = Image.FromFile("assets/Bigger.png");
+        }
+
+        private void OnTick(object sender, EventArgs e)
+        {
+            time = DateTime.Now;
+            Redraw();
+            if(sec == 1)
+            {
+                sec--;
+            }
+            else if(sec == 0)
+            {
+                closeButton.Visible = false;
+                pinButton.Visible = false;
+                biggerButton.Visible = false;
+                smallerButton.Visible = false;
+            }
+        }
+
+        private void OnMouseEnter(object sender, EventArgs e)
+        {
+            closeButton.Visible = true;
+            pinButton.Visible = true;
+            biggerButton.Visible = true;
+            smallerButton.Visible = true;
+            sec = 2;
+        }
+
+        private void OnMouseLeave(object sender, EventArgs e)
+        {
+            sec = 1;
+        }
+
+        private void OnClickClose(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void OnMouseEnterClose(object sender, EventArgs e)
+        {
+            sec = 2;
+            closeButton.Image = Image.FromFile("assets/CloseOver.png");
+        }
+
+        private void OnMouseLeaveClose(object sender, EventArgs e)
+        {
+            sec = 1;
+            closeButton.Image = Image.FromFile("assets/Close.png");
+        }
+
+        private void OnClickPin(object sender, EventArgs e)
+        {
+            if(pinned == false)
+            {
+                this.TopMost = true;
+                pinned = true;
+                pinButton.Image = Image.FromFile("assets/UnpinOver.png");
+            }
+            else
+            {
+                this.TopMost = false;
+                pinned = false;
+                pinButton.Image = Image.FromFile("assets/PinOver.png");
+            }
+        }
+
+        private void OnMouseEnterPin(object sender, EventArgs e)
+        {
+            sec = 2;
+            if(pinned == false)
+            {
+                pinButton.Image = Image.FromFile("assets/PinOver.png");
+            }
+            else
+            {
+                pinButton.Image = Image.FromFile("assets/UnpinOver.png");
+            }
+        }
+
+        private void OnMouseLeavePin(object sender, EventArgs e)
+        {
+            sec = 1;
+            if (pinned == false)
+            {
+                pinButton.Image = Image.FromFile("assets/Pin.png");
+            }
+            else
+            {
+                pinButton.Image = Image.FromFile("assets/Unpin.png");
+            }
+        }
+
+        private void OnMouseDownMove(object sender, MouseEventArgs e)
+        {
+            xOffset = MousePosition.X - this.Left;
+            yOffset = MousePosition.Y - this.Top;
+        }
+
+        private void OnMouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left = MousePosition.X - xOffset;
+                this.Top = MousePosition.Y - yOffset;
+            }
+        }
+
+        private void OnClickSmaller(object sender, EventArgs e)
+        {
+            if (clockRes > 100)
+            {
+                clockRes -= 50;
+                g.Clear(Color.FromArgb(64, 0, 64));
+                this.Left += 50;
+                closeButton.Left -= 50;
+                pinButton.Left -= 50;
+                biggerButton.Left -= 50;
+                smallerButton.Left -= 50;
+                Redraw();
+            }
+        }
+
+        private void OnMouseEnterSmaller(object sender, EventArgs e)
+        {
+            sec = 2;
+            smallerButton.Image = Image.FromFile("assets/SmallerOver.png");
+        }
+
+        private void OnMouseLeaveSmaller(object sender, EventArgs e)
+        {
+            sec = 1;
+            smallerButton.Image = Image.FromFile("assets/Smaller.png");
+        }
+
+        private void OnClickBigger(object sender, EventArgs e)
+        {
+            if (clockRes < 1000)
+            {
+                clockRes += 50;
+                g.Clear(Color.FromArgb(64, 0, 64));
+                this.Left -= 50;
+                closeButton.Left += 50;
+                pinButton.Left += 50;
+                biggerButton.Left += 50;
+                smallerButton.Left += 50;
+                Redraw();
+            }
+        }
+
+        private void OnMouseEnterBigger(object sender, EventArgs e)
+        {
+            sec = 2;
+            biggerButton.Image = Image.FromFile("assets/BiggerOver.png");
+        }
+
+        private void OnMouseLeaveBigger(object sender, EventArgs e)
+        {
+            sec = 1;
+            biggerButton.Image = Image.FromFile("assets/Bigger.png");
+        }
+    }
+}
